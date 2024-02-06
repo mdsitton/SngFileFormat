e19eb1110b6e82bfef1a2d4167f963dbf193bb42
Nathan <nathanhurst03@gmail.com>
2024-02-05 23:07:40 -0700

    Add a bunch more known metadata keys

--- SngTool/SngCli/KnownKeys.cs
+++ SngTool/SngCli/KnownKeys.cs
@@ -9,9 +9,15 @@ namespace SngCli
 {
     public enum IniValueType
     {
+        // Primitive
         Integer,
         String,
         Boolean,
+
+        // Complex
+        ColorCode,
+        IntegerPair,
+        ProGuitarTuning,
     }
 
     public static class KnownKeys
@@ -22,30 +28,50 @@ namespace SngCli
             ( "artist",               IniValueType.String ),
             ( "album",                IniValueType.String ),
             ( "genre",                IniValueType.String ),
+            ( "sub_genre",            IniValueType.String ),
             ( "year",                 IniValueType.String ),
 
             ( "charter",              IniValueType.String ),
             ( "frets",                IniValueType.String ),
-            ( "icon",                 IniValueType.String ),
-            ( "loading_phrase",       IniValueType.String ),
+            ( "version",              IniValueType.Integer ),
 
             ( "diff_band",            IniValueType.Integer ),
+
             ( "diff_guitar",          IniValueType.Integer ),
-            ( "diff_rhythm",          IniValueType.Integer ),
-            ( "diff_guitar_coop",     IniValueType.Integer ),
-            ( "diff_bass",            IniValueType.Integer ),
-            ( "diff_drums",           IniValueType.Integer ),
-            ( "diff_drums_real",      IniValueType.Integer ),
-            ( "diff_keys",            IniValueType.Integer ),
             ( "diff_guitarghl",       IniValueType.Integer ),
-            ( "diff_bassghl",         IniValueType.Integer ),
+            ( "diff_guitar_coop",     IniValueType.Integer ),
             ( "diff_guitar_coop_ghl", IniValueType.Integer ),
+            ( "diff_guitar_real",     IniValueType.Integer ),
+            ( "diff_guitar_real_22",  IniValueType.Integer ),
+
+            ( "diff_rhythm",          IniValueType.Integer ),
             ( "diff_rhythm_ghl",      IniValueType.Integer ),
 
+            ( "diff_bass",            IniValueType.Integer ),
+            ( "diff_bassghl",         IniValueType.Integer ),
+            ( "diff_bass_real",       IniValueType.Integer ),
+            ( "diff_bass_real_22",    IniValueType.Integer ),
+
+            ( "diff_drums",           IniValueType.Integer ),
+            ( "diff_drums_real",      IniValueType.Integer ),
+            ( "diff_drums_real_ps",   IniValueType.Integer ),
+
+            ( "diff_keys",            IniValueType.Integer ),
+            ( "diff_keys_real",       IniValueType.Integer ),
+            ( "diff_keys_real_ps",    IniValueType.Integer ),
+
+            ( "diff_vocals",          IniValueType.Integer ),
+            ( "diff_vocals_harm",     IniValueType.Integer ),
+
+            ( "diff_dance",           IniValueType.Integer ),
+
             ( "song_length",          IniValueType.Integer ),
-            ( "preview_start_time",   IniValueType.Integer ),
-            ( "video_start_time",     IniValueType.Integer ),
             ( "delay",                IniValueType.Integer ),
+            ( "preview_start_time",   IniValueType.Integer ),
+            ( "preview_end_time",     IniValueType.Integer ),
+            ( "preview",              IniValueType.IntegerPair ),
+            ( "video_start_time",     IniValueType.Integer ),
+            ( "video_end_time",       IniValueType.Integer ),
 
             ( "playlist_track",       IniValueType.Integer ),
             ( "album_track",          IniValueType.Integer ),
@@ -54,14 +80,66 @@ namespace SngCli
             ( "playlist",             IniValueType.String ),
             ( "sub_playlist",         IniValueType.String ),
 
-            ( "multiplier_note",      IniValueType.Integer ),
-            ( "hopo_frequency",       IniValueType.Integer ),
-            ( "eighthnote_hopo",      IniValueType.Boolean ),
+            ( "cover",                IniValueType.String ),
+            ( "icon",                 IniValueType.String ),
+            ( "background",           IniValueType.String ),
+            ( "video",                IniValueType.String ),
+            ( "video_loop",           IniValueType.Boolean ),
+
+            ( "link_name_a",          IniValueType.String ),
+            ( "link_name_b",          IniValueType.String ),
+            ( "banner_link_a",        IniValueType.String ),
+            ( "banner_link_b",        IniValueType.String ),
+
+            ( "cassettecolor",        IniValueType.ColorCode ),
+            ( "tags",                 IniValueType.String ),
+            ( "loading_phrase",       IniValueType.String ),
+
+            ( "tutorial",             IniValueType.Boolean ),
+            ( "boss_battle",          IniValueType.Boolean ),
+
+            ( "unlock_id",            IniValueType.String ),
+            ( "unlock_require",       IniValueType.String ),
+            ( "unlock_text",          IniValueType.String ),
+            ( "unlock_completed",     IniValueType.Integer ),
+
+            ( "sustain_cutoff_threshold", IniValueType.Integer ),
+            ( "multiplier_note",          IniValueType.Integer ),
+            ( "star_power_note",          IniValueType.Integer ),
+            ( "hopo_frequency",           IniValueType.Integer ),
+            ( "eighthnote_hopo",          IniValueType.Boolean ),
 
             ( "pro_drums",            IniValueType.Boolean ),
             ( "five_lane_drums",      IniValueType.Boolean ),
+            ( "drum_fallback_blue",   IniValueType.Boolean ),
+            ( "vocal_gender",         IniValueType.Boolean ),
+
+            ( "sysex_slider",         IniValueType.Boolean ),
+            ( "sysex_high_hat_ctrl",  IniValueType.Boolean ),
+            ( "sysex_rimshot",        IniValueType.Boolean ),
+            ( "sysex_open_bass",      IniValueType.Boolean ),
+            ( "sysex_pro_slide",      IniValueType.Boolean ),
+
+            ( "guitar_type",          IniValueType.Integer ),
+            ( "bass_type",            IniValueType.Integer ),
+            ( "kit_type",             IniValueType.Integer ),
+            ( "keys_type",            IniValueType.Integer ),
+            ( "dance_type",           IniValueType.Integer ),
+
+            ( "real_guitar_tuning",    IniValueType.ProGuitarTuning ),
+            ( "real_guitar_22_tuning", IniValueType.ProGuitarTuning ),
+            ( "real_bass_tuning",      IniValueType.ProGuitarTuning ),
+            ( "real_bass_22_tuning",   IniValueType.ProGuitarTuning ),
+
+            ( "real_keys_lane_count_right", IniValueType.Integer ),
+            ( "real_keys_lane_count_left",  IniValueType.Integer ),
+
+            ( "eof_midi_import_drum_accent_velocity", IniValueType.Integer ),
+            ( "eof_midi_import_drum_ghost_velocity",  IniValueType.Integer ),
+
             ( "end_events",           IniValueType.Boolean ),
             ( "modchart",             IniValueType.Boolean ),
+            ( "lyrics",               IniValueType.Boolean ),
         };
 
         private static readonly HashSet<string> knownMetadataKeys = knownMetadata.Select((v) => v.key).ToHashSet();
@@ -70,6 +148,8 @@ namespace SngCli
         {
             ( "frets", "charter" ),
             ( "track", "album_track" ),
+            ( "star_power_note", "multiplier_note" ),
+            ( "pro_drum", "pro_drums" ),
         };
 
         private static readonly List<(string key, Func<string, string?> fixup)> metadataFixups = new()
@@ -92,9 +172,67 @@ namespace SngCli
                     IniValueType.Integer => int.TryParse(value, out _),
                     IniValueType.String => !string.IsNullOrEmpty(value),
                     IniValueType.Boolean => bool.TryParse(value, out _) ||
-                        (int.TryParse(value, out int intBool) && intBool is 0 or 1),                            
+                        (int.TryParse(value, out int intBool) && intBool is 0 or 1),
+
+                    IniValueType.ColorCode => value.StartsWith('#') &&
+                        value.AsSpan().IndexOfAny("0123456789ABCDEFabcdef") < 0,
+                    IniValueType.IntegerPair => ValidateIntegerPair(value),
+                    IniValueType.ProGuitarTuning => ValidateProGuitarTuning(value),
+
                     _ => throw new NotImplementedException($"Unrecognized IniValueType {type}")
                 };
+
+                // preview = 35000 65000
+                static bool ValidateIntegerPair(ReadOnlySpan<char> text)
+                {
+                    int splitIndex = text.IndexOf(' ');
+                    if (splitIndex < 0)
+                        return false;
+
+                    var first = text.Slice(0, splitIndex);
+                    var second = text.Slice(splitIndex + 1);
+
+                    return int.TryParse(first, out _) && int.TryParse(second, out _);
+                }
+
+                // real_guitar_tuning = 0 0 0 0 0 0 "Standard tuning"
+                // real_guitar_22_tuning = 0 2 5 7 10 10
+                // real_bass_tuning = -2 0 0 0 "Drop D tuning"
+                // real_bass_22_tuning = 0 0 0 0 0
+                static bool ValidateProGuitarTuning(ReadOnlySpan<char> text)
+                {
+                    // Go through each number
+                    int numbers = 0;
+                    int splitIndex;
+                    while ((splitIndex = text.IndexOf(' ')) >= 0)
+                    {
+                        var split = text.Slice(0, splitIndex);
+                        if (!int.TryParse(split, out _))
+                            break;
+
+                        numbers++;
+                        text = text.Slice(splitIndex + 1);
+                    }
+
+                    // Handle remaining text
+                    if (int.TryParse(text, out _))
+                    {
+                        numbers++;
+                    }
+                    else
+                    {
+                        // Check for quoted segment
+                        int quoteStartIndex = text.IndexOf('"');
+                        if (quoteStartIndex >= 0)
+                        {
+                            var quoteEndSearch = text.Slice(quoteStartIndex + 1);
+                            if (quoteEndSearch.IndexOf('"') < 0)
+                                return false;
+                        }
+                    }
+
+                    return numbers is >= 4 and <= 6;
+                }
 
                 if (!valid)
                 {
